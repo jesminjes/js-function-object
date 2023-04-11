eade5f050aa52d2505d7d4145751d1b909850888
jesminjes <mstjesmina56@gmail.com>
2023-04-11 17:15:16 +0600

    function.js

---
+++ function.js
@@ -0,0 +1,13 @@
+// function declaration
+function startFan(){
+    console.log('dtand up');
+    console.log('walk towards the switch')
+    console.log('press thr switch')
+}
+
+// call the function
+startFan();
+console.log('waking up in the morning')
+startFan();
+startFan();
+startFan();
