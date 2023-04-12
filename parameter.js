f8f4960581cfde4ae723e6198a9f2ee1e3fd1a8d
jesminjes <mstjesmina56@gmail.com>
2023-04-12 23:04:48 +0600

    parameter

--- parameter.js
+++ parameter.js
@@ -2,4 +2,17 @@ function bringSingara(money){
     console.log('mama singara den');
     console.log(money);
 }
-bringSingara(100);
+var taka = 300;
+// bringSingara(100);
+
+function add(num1, num2){
+    console.log('going to add:', num1, num2);
+
+}
+add(125);
+function sum(a, b, c, d, e){
+    console.log(a, b, c, d, e);
+    var sum = a + b + c + d + e;
+    console.log(sum);
+}
+sum(15, 98, 56)
