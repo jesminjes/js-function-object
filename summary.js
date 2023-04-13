4223c431e94c363e25cb42bb92728f90ee0e28a9
jesminjes <mstjesmina56@gmail.com>
2023-04-13 12:27:22 +0600

    summary.js

---
+++ summary.js
@@ -0,0 +1,19 @@
+const color = 'yellow';
+
+if (color === 'green'){
+    console.log('you are a greeen friend');
+}
+else if (color === 'blue'){
+    console.log('you are a greeen friend');
+}
+else if (color === 'red'){
+    console.log('you are a red friend');
+}
+else if (color === 'white'){
+    console.log('you are a white friend');
+}
+else if (color === "yellow"){
+    console.log('you are a yellow friend');
+}
+// switch
+
